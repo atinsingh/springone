bb1846384a06616f07fd9e4e94389735061c4913
Atin Singh <atin@pragra.io>
2024-12-14 12:53:04 -0500

    Updated

--- src/main/java/co/pragra/learning/first_spring_proj/FirstSpringProjApplication.java
+++ src/main/java/co/pragra/learning/first_spring_proj/FirstSpringProjApplication.java
@@ -1,5 +1,6 @@
 package co.pragra.learning.first_spring_proj;
 
+import co.pragra.learning.first_spring_proj.dec14.domain.Person;
 import co.pragra.learning.first_spring_proj.dec14.service.InsuranceService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -11,6 +12,10 @@ public class FirstSpringProjApplication {
 		ApplicationContext context = new FileSystemXmlApplicationContext("classpath:beans.xml");
 		InsuranceService insuranceService = context.getBean("insuranceService", InsuranceService.class);
 		System.out.println(insuranceService.insure().getQuote());
+
+		Person person = context.getBean("person", Person.class);
+		System.out.println("person = " + person);
+		
 	}
 
 
--- src/main/java/co/pragra/learning/first_spring_proj/dec14/domain/Person.java
+++ src/main/java/co/pragra/learning/first_spring_proj/dec14/domain/Person.java
@@ -1,2 +1,27 @@
-package co.pragra.learning.first_spring_proj.dec14.domain;public class Person {
+package co.pragra.learning.first_spring_proj.dec14.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+
+@ToString
+public class Person {
+    private String firstName;
+    private int age;
+    private double income;
+
+    public Person(String firstName, int age, double income) {
+        this.firstName = firstName;
+        this.age = age;
+        this.income = income;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", age=" + age +
+                ", income=" + income +
+                '}';
+    }
 }
